eebc00886e01b6f8280b93e2aedbf749c5a919e2
Lucas Gasperin <luscagape@gmail.com>
2025-06-23 11:24:17 -0300

    patch: updates action and build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,13 @@ mkdir -p package
 zip package.zip lambda_function.py
 
 echo "Pacote criado: package.zip"
+
+# Remove e cria o diretório function
+rm -rf function
+mkdir function
+
+# Configura o diretório function
+mv package.zip function
+(cd function && unzip package.zip)
+
+echo "Pacote movido para o diretório function"
